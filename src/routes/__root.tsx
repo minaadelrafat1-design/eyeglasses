6ba584e45dc234b16c6520664b6c03f5f59e7dbd
minaadelrafat1-design <minaadelrafat1@gmail.com>
2026-09-24 17:05:55 +0300

    logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -101,14 +101,13 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       },
       { property: "og:type", content: "website" },
       { name: "twitter:card", content: "summary_large_image" },
-      { name: "twitter:site", content: "@Lovable" },
     ],
     links: [
       {
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
     ],
   }),
   shellComponent: RootShell,
